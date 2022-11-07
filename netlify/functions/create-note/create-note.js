aa637f19eb0622a6723baa957a07dccfad677c6f
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-07 17:46:54 +0100

    refactor and add planRelease

--- netlify/functions/create-note/create-note.js
+++ netlify/functions/create-note/create-note.js
@@ -1,89 +1,81 @@
-const process = require('process')
+const process = require("process");
 
-const sanityClient = require('@sanity/client')
+const sanityClient = require("@sanity/client");
 
 const client = sanityClient({
   projectId: process.env.SANITY_PROJECT,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_TOKEN,
-  apiVersion: '2022-01-01',
+  apiVersion: "2022-01-01",
   useCdn: false,
-})
+});
 
 const handler = async (event, context) => {
+  let newNote = {
+    _type: "note",
+    title: event.queryStringParameters.title,
+    content: event.queryStringParameters.content,
+    // TODO: image: image,
+    domain: "litit-demo.netlify.app", // TODO: event.queryStringParameters.domain
+    preset: event.queryStringParameters.preset,
+    status: event.queryStringParameters.status,
 
-  const title = event.queryStringParameters.title
-  const content = event.queryStringParameters.content
-  const domain = event.queryStringParameters.domain
-  const preset = event.queryStringParameters.preset
-  const status = event.queryStringParameters.status
-  const dateFrom = event.queryStringParameters.dateFrom
-  const dateTo = event.queryStringParameters.dateTo
+    belongsTo: {
+      _type: "reference",
+      _ref: uId,
+    },
+  };
 
-  const uId = context.clientContext.user.sub
-  const uRoles = context.clientContext.user.app_metadata.roles
+  /* TODO: Plan release date */
+  if (event.queryStringParameters.planRelease) {
+    newNote.dateFrom = event.queryStringParameters.dateFrom;
+    newNote.dateTo = event.queryStringParameters.dateTo;
+  } 
 
+  const uId = context.clientContext.user.sub;
+  const uRoles = context.clientContext.user.app_metadata.roles;
 
   /* no user, no go */
   if (!uId) {
-    console.log('No user!')
+    console.log("No user!");
     return {
       statusCode: 401,
       body: JSON.stringify({
-        data: 'no go',
+        data: "no go",
       }),
-    }
+    };
   }
 
   /* no basic role, no go */
-  if (uRoles[0] !== 'basic') {
-    console.log('No basic role!')
+  if (uRoles[0] !== "basic") {
+    console.log("No basic role!");
     return {
       statusCode: 401,
       body: JSON.stringify({
-        data: 'no go',
+        data: "no go",
       }),
-    }
-  }
-
-  const newNote = {
-    _type: 'note',
-    title: title,
-    content: content,
-    // image: image,
-    domain: "litit-demo.netlify.app",
-    preset: preset,
-    status: status,
-    // dateFrom: dateFrom,
-    // dateTo: dateTo,
-    
-    belongsTo: {
-      _type: 'reference',
-      _ref: uId
-    }
+    };
   }
 
   try {
-    
     const result = await client.create(newNote).then((res) => {
-      console.log('RESULT FROM SANITY: ', res)
-    })
+      console.log("RESULT FROM SANITY: ", res);
+    });
 
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(result)
-    }
-  } 
-  
-  catch (error) {
-    console.log(error)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log(error);
     return {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
       statusCode: 500,
-      body: error.responseBody || JSON.stringify({ error: 'An error occurred' }),
-    }
+      body:
+        error.responseBody || JSON.stringify({ error: "An error occurred" }),
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
