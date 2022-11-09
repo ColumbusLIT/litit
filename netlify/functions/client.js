715680b221a3fda06de9cc64b7d4c6afa8f1e2c1
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-09 15:19:43 +0100

    fix syntax

--- netlify/functions/client.js
+++ netlify/functions/client.js
@@ -1,12 +1,11 @@
 const sanityClient = require("@sanity/client");
-
-module.exports = { handler };
-{
-  client = sanityClient({
-    projectId: process.env.SANITY_PROJECT,
-    dataset: process.env.SANITY_DATASET,
-    token: process.env.SANITY_TOKEN,
-    apiVersion: "2022-01-01",
-    useCdn: false,
-  });
-}
+const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT,
+  dataset: process.env.SANITY_DATASET,
+  token: process.env.SANITY_TOKEN,
+  apiVersion: "2022-01-01",
+  useCdn: false,
+});
+module.exports = {
+  client,
+};
