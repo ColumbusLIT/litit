ea6ac85204cbe48880dfd944686df32b19a0bb71
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-09 15:17:31 +0100

    module export fix

--- netlify/functions/client.js
+++ netlify/functions/client.js
@@ -1,9 +1,12 @@
 const sanityClient = require("@sanity/client");
 
-export const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT,
-  dataset: process.env.SANITY_DATASET,
-  token: process.env.SANITY_TOKEN,
-  apiVersion: "2022-01-01",
-  useCdn: false,
-});
+module.exports = { handler };
+{
+  client = sanityClient({
+    projectId: process.env.SANITY_PROJECT,
+    dataset: process.env.SANITY_DATASET,
+    token: process.env.SANITY_TOKEN,
+    apiVersion: "2022-01-01",
+    useCdn: false,
+  });
+}
